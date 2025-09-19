e9519ccb48aef9ba88686651bc4829c7701bbb27
TarunTalan <taruntalan12@gmail.com>
2025-09-19 23:14:37 +0530

    create a circle and give it a horizontal velocity

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext('2d');
-canvas.width = innerWidth;
 canvas.height = innerHeight;
+canvas.width = innerWidth;
+
+
+let x = Math.random() * innerWidth;
+let y = Math.random() * innerHeight;
+let dx = 2;
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = "black";
+    c.beginPath();
+    c.arc(x, y, 20, 0, 2 * Math.PI);
+    c.fill();
+    c.closePath();
+    x += dx;
+}
+animate();
