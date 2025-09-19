4ccb84327648d6b2cafdf561694b87023bc6c9fb
TarunTalan <taruntalan12@gmail.com>
2025-09-19 23:22:05 +0530

    make the circle to move in both direction and bounce back stricking the walls

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ canvas.width = innerWidth;
 let x = Math.random() * innerWidth;
 let y = Math.random() * innerHeight;
 let dx = 2;
+let dy = 2;
+let r = 20;
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
     c.fillStyle = "black";
     c.beginPath();
-    c.arc(x, y, 20, 0, 2 * Math.PI);
+    c.arc(x, y, r, 0, 2 * Math.PI);
     c.fill();
     c.closePath();
+    if((x+r >= innerWidth) || (x<=r)){
+        dx = -dx;
+    }
+    if((y+r >= innerHeight) || (y<=r)){
+        dy = -dy;
+    }
     x += dx;
+    y += dy;
 }
 animate();
