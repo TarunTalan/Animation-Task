7af5682ba470d69700a5df71e01a8ac20f72444f
TarunTalan <taruntalan12@gmail.com>
2025-09-20 00:46:24 +0530

    created ball class and give its object motion in x and y. Also defined boundaries to bounce back

--- script2.js
+++ script2.js
@@ -2,3 +2,39 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext('2d');
 canvas.height = innerHeight;
 canvas.width = innerWidth;
+
+
+class Ball {
+    constructor(x, y, dx, dy, r) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.r = r;
+    }
+    makeBall() {
+        c.fillStyle = "black";
+        c.beginPath();
+        c.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        c.fill();
+    }
+    updateBall() {
+        if ((this.x + this.r + this.dx > innerWidth) || (this.x <= this.r)) {
+            this.dx = -this.dx;
+        }
+        if ((this.y + this.r + this.dy > innerHeight) || (this.y <= this.r)) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.makeBall();
+    }
+}
+
+let ball = new Ball(100,100,2,2,10);
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    ball.updateBall();
+}
+animate();
