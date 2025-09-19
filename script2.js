5c73418edc5bf521e756405693f701870cb03178
TarunTalan <taruntalan12@gmail.com>
2025-09-20 01:11:50 +0530

    add eventlistner to init animation

--- script2.js
+++ script2.js
@@ -44,15 +44,18 @@ class Ball {
 }
 
 let ballArr = [];
-let numball = 400;
-for (let i = 0; i < numball; i++) {
+window.addEventListener('click', () => {
+    ballArr = [];
+    let numball = 400;
+    for(let i = 0; i<numball; i++) {
     let r = randomNum(10, 20);
     let dx = randomNum(-2, 2);
     let dy = randomNum(0, 2);
     let x = randomNum(r, innerWidth - r);
-    let y = randomNum(r + dy, innerHeight - 2*r);
+    let y = randomNum(r+dy, innerHeight - 2*r);
     ballArr.push(new Ball(x, y, dx, dy, r));
-};
+}
+});
 
 function animate() {
     requestAnimationFrame(animate);
