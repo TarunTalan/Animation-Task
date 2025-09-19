52949dc26c617df52640f101bc0e294611646e18
TarunTalan <taruntalan12@gmail.com>
2025-09-19 23:33:16 +0530

    make the circle object using Circle class

--- script.js
+++ script.js
@@ -4,27 +4,46 @@ canvas.height = innerHeight;
 canvas.width = innerWidth;
 
 
+
+
+
+class Circle {
+    constructor(x, y, dx, dy, r) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.r = r;
+    }
+    makeCircle() {
+        c.fillStyle = "blue";
+        c.beginPath();
+        c.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        c.fill();
+    }
+    updateCircle() {
+        if ((this.x + this.r >= innerWidth) || (this.x <= this.r)) {
+            this.dx = -this.dx;
+        }
+        if ((this.y + this.r >= innerHeight) || (this.y <= this.r)) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.makeCircle();
+    }
+}
+
 let x = Math.random() * innerWidth;
 let y = Math.random() * innerHeight;
 let dx = 2;
 let dy = 2;
 let r = 20;
+let  circle = new Circle(x,y,dx,dy,r);
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.fillStyle = "black";
-    c.beginPath();
-    c.arc(x, y, r, 0, 2 * Math.PI);
-    c.fill();
-    c.closePath();
-    if((x+r >= innerWidth) || (x<=r)){
-        dx = -dx;
-    }
-    if((y+r >= innerHeight) || (y<=r)){
-        dy = -dy;
-    }
-    x += dx;
-    y += dy;
+    circle.updateCircle();
 }
 animate();
