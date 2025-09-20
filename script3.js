3da26ce857be1505bdba75ae3f8234edd80cb4dd
TarunTalan <taruntalan12@gmail.com>
2025-09-20 13:48:06 +0530

    adding motion to image and adding a factor to control frame rate change

--- script3.js
+++ script3.js
@@ -8,10 +8,23 @@ const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
+let FrameX = 0;
+let FrameY = 0;
+let gameFrame = 0;
+let staggerFrames = 5;
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.drawImage(playerImage,spriteWidth,spriteHeight,spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight);
+    c.drawImage(playerImage, FrameX * spriteWidth, FrameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames == 0) {
+        if (FrameX < 6) {
+            FrameX++;
+        } else {
+            FrameX = 0;
+        }
+    }
+    gameFrame++;
+
 }
 animate();
